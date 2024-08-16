72044416960e032f4cee8bacac6ce277f2eb2c68
Gabriel de Paula Alvarenga Adeodato <111003605+GabrielAdeodato@users.noreply.github.com>
2024-08-16 10:46:42 -0300

    Add files via upload

---
+++ variaveisOperadores.js
@@ -0,0 +1,12 @@
+let valor1 = 2;
+let valor2 = 4;
+
+resultadoSoma = valor1 + valor2;
+resultadoSubtracao = valor1 - valor2;
+resultadoDivisao = valor1 / valor2;
+resultadoMultiplicao = valor1 * valor2;
+
+console.log(`soma: ${valor1} + ${valor2} = ${resultadoSoma}`);
+console.log(`subtração: ${valor1} - ${valor2} = ${resultadoSubtracao}`);
+console.log(`multiplicação: ${valor1} * ${valor2} = ${resultadoMultiplicao}`);
+console.log(`divisão: ${valor1} / ${valor2} = ${resultadoDivisao}`);
