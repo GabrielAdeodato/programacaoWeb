c521ac3b104d6b9f02d8776d3f9c9ac9641edf11
Gabriel de Paula Alvarenga Adeodato <111003605+GabrielAdeodato@users.noreply.github.com>
2024-08-30 15:25:29 -0300

    Update verificarNumeroPrimo.js

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -1,1 +1,31 @@
+function verificarNumeroPrimo(num) {
+let primo = true;
 
+if (num <= 1) {
+    primo = false;
+    return console.log(primo);
+}
+if (num % 2 == 0 && num != 2 && num) {
+    primo = false;
+    return console.log(primo);
+}
+
+for (let i = 3; i < num; i += 2) {
+    if (num % i == 0) {
+        primo = false;
+        return console.log(primo);
+    }
+}
+return console.log(primo);;
+}
+
+verificarNumeroPrimo(0);
+verificarNumeroPrimo(1);
+verificarNumeroPrimo(2);
+verificarNumeroPrimo(3);
+verificarNumeroPrimo(7);
+verificarNumeroPrimo(83);
+verificarNumeroPrimo(100);
+verificarNumeroPrimo(991);
+verificarNumeroPrimo(104729);
+verificarNumeroPrimo(14348907);
